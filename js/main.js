4ef837836366f673efd691d64804ef90aba4b649
xTrebliG <gil@crankshiftnyc.com>
2017-01-21 02:12:53 -0500

    Added clickable info boxes for three Rs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(function() {
-    $('.chev-hld').click(function(){
-        $(this).addClass('active');
+    $('.chev-hld').on('touch click', function(){
+        $('.chev-hld').removeClass('active');
+        $('.collapse').collapse('hide');
+        var theChev = $(this);
+        $('.collapse').on('shown.bs.collapse', function () {
+            $('.chev-hld').removeClass('active');
+            theChev.addClass('active');
+        });
+        var thisItem = $(this);
+        setTimeout( function(){
+            var offset = thisItem.offset();
+            offset.left -= 20;
+            offset.top -= 20;
+            $('html, body').animate({
+                scrollTop: offset.top,
+                scrollLeft: offset.left
+            }, 800);
+        },500);
+    });
+    $(".three-r-nav").on('touch click', function(){
+        var title = ".chev-hld."+$(this).attr('title');
+        $(title).trigger('click');
+
     })
 });
