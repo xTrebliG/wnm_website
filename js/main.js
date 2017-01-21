269b9cd55606c5d866da36f3ff3abe562fd531c6
xTrebliG <gil@crankshiftnyc.com>
2017-01-21 03:46:02 -0500

    Added back to top btn, added contact section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(function() {
+	var offset = 300,
+		offset_opacity = 1200,
+		scroll_top_duration = 700,
+		$back_to_top = $('.cd-top');
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
     $('.chev-hld').on('touch click', function(){
         $('.chev-hld').removeClass('active');
         $('.collapse').collapse('hide');
@@ -9,12 +26,12 @@ $(function() {
         });
         var thisItem = $(this);
         setTimeout( function(){
-            var offset = thisItem.offset();
-            offset.left -= 20;
-            offset.top -= 20;
+            var offsetChev = thisItem.offset();
+            offsetChev.left -= 20;
+            offsetChev.top += 70;
             $('html, body').animate({
-                scrollTop: offset.top,
-                scrollLeft: offset.left
+                scrollTop: offsetChev.top,
+                scrollLeft: offsetChev.left
             }, 800);
         },500);
     });
